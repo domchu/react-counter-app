7e7ab87335781838903d986fc2523020a5113896
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-08-23 00:50:15 +0100

    added the onclick function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Button from "./Button";
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleIncreaement = () => setCount(count + 1);
+  // const handleIncreasement = () => setCount(count + 1);
 
   return (
     <div div className="container my-5">
@@ -13,6 +13,24 @@ function App() {
           <h1>Counter App</h1>
           <div className="my-5">
             <h2 className="my-5 ">{count}</h2>
+            <Button
+              className="btn btn-success mx-3"
+              text="Increasement"
+              onClick={() => setCount(count + 1)}
+              disabled={false}
+            />
+            <Button
+              className="btn btn-danger mx-3"
+              text="Decreasement"
+              onClick={() => setCount(count - 1)}
+              disabled={count === 0}
+            />
+            <Button
+              className="btn btn-secondary mx-3"
+              text="Reset"
+              onClick={() => setCount(0)}
+              disabled={count === 0}
+            />
 
             {/* <Button text="something" onClick={() => { }} className="sdsd" disabled={true} /> */}
 
--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Button = () => {
-  return <button></button>;
+const Button = (props) => {
+  return (
+    <button
+      className={props.className}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.text}
+    </button>
+  );
 };
 
 export default Button;
